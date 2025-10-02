472f3bff9a8b35bca6c64d2031fe8db7c3a37061
Lior Shefer <liorshe@wix.com>
2025-10-02 09:46:41 +0300

    setting up local ollama integration

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,3 +1,4 @@
+import { ollama } from 'ai-sdk-ollama';
 import { convertToModelMessages, streamText, UIMessage } from 'ai';
 
 // Allow streaming responses up to 30 seconds
@@ -5,14 +6,12 @@ export const maxDuration = 30;
 
 export async function POST(req: Request) {
     const {
-        messages,
-        model,
-        webSearch,
-    }: { messages: UIMessage[]; model: string; webSearch: boolean } =
+        messages,        
+    }: { messages: UIMessage[] } =
         await req.json();
 
     const result = streamText({
-        model: webSearch ? 'perplexity/sonar' : model,
+        model: ollama('llama2:chat'),
         messages: convertToModelMessages(messages),
         system:
             'You are a helpful assistant that can answer questions and help with tasks',
